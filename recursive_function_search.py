830482fc9c087e50c7507b41c93aa06c18aa5c1d
mike <mike@github>
2024-11-13 08:17:02 -0500

    init

---
+++ recursive_function_search.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import idautils
+import idaapi
+import idc
+
+# Recursive function to search for the target function
+def _find_path_to_target_function(current_function, target_function, path, visited):
+    # Add the current function to the path
+    path.append(current_function)
+
+    # Mark the current function as visited
+    visited.add(current_function)
+
+    # Check if we've reached the target function
+    if idc.get_func_name(current_function) == target_function:
+        # Print the path to the target
+        print("Path to", target_function, ":", " -> ".join([idc.get_func_name(addr) for addr in path]))
+        # Remove the current function from the path and return
+        path.pop()
+        return
+
+    # Loop through all call instructions in the current function
+    for ref in idautils.FuncItems(current_function):
+        if idc.print_insn_mnem(ref) == "call":
+            # Get the address of the called function
+            target = idc.get_operand_value(ref, 0)
+            # Check if this is a valid function and hasn't been visited
+            if idc.get_func_name(target) and target not in visited:
+                # Recursively search from the called function
+                _find_path_to_target_function(target, target_function, path, visited)
+
+    # Remove the current function from the path when going back
+    path.pop()
+
+
+def find_path_to_target_function(target):
+    # Find the address of the root function (main in this case)
+    root_function = idc.get_screen_ea()
+    # Check if the main function was found
+    if root_function != idaapi.BADADDR:
+        print("Starting search from current function...")
+        _find_path_to_target_function(root_function, target, [], set())
+    else:
+        print("root function ea not found.")
